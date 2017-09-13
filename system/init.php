e4253099f77577a519b7873d0856ce4566044daf
fesiong <tpyzlxy@163.com>
2017-09-13 14:09:02 +0800

    bugfix

--- system/init.php
+++ system/init.php
@@ -45,7 +45,7 @@ if (defined('SAE_TMP_PATH'))
 
 define('ROOT_PATH', dirname(dirname(__FILE__)) . '/');
 
-@ini_set('display_errors', '1');
+@ini_set('display_errors', '0');
 
 if (defined('IN_SAE'))
 {
